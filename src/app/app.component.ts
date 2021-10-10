d3c75c774a37b8e8c7dea61515dcbe74a25e173e
Caroline Sarre <anacarolinesarre@gmail.com>
2021-10-10 09:33:33 -0300

    coreçao do foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import 'foundation-sites';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-investimento';
+  title = 'investimentos';
+  ngOnInit() {
+    $(document).ready(function () {
+      $(document).foundation();
+    });
+  }
+  constructor(){}
 }
